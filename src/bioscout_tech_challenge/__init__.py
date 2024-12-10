8d4818688194d1c4f3c17c86d412485144a833e2
Zach Milgate <85665417+VanDammeHouten@users.noreply.github.com>
2024-12-11 09:59:56 +1100

    Enhance weather data processing and CLI functionality
    - Updated Exploration notebook to improve execution tracking and added new outputs for weather data processing.
    - Refactored CLI commands for merging and flattening weather data, introducing a new command group for better organization.
    - Enhanced utility functions in weather.py for processing sensor data, including improved error handling and data type conversions.
    - Updated sensor_schema.json to include column types for better data validation.
    - Refactored package structure to streamline imports and improve code organization.

--- src/bioscout_tech_challenge/__init__.py
+++ src/bioscout_tech_challenge/__init__.py
@@ -5,49 +5,14 @@ BioScout Technical Challenge
 A package for processing and analyzing weather and sensor data.
 """
 
-from .utils import (
-    read_csv_file,
-    save_csv_file,
-    identify_header,
-    combine_csv_files,
-    find_csv_files,
-    read_json_file,
-    merge_weather_data,
-    get_na_rows,
-    expand_extra_information,
-    # get_timezone_from_utc_offset,
-    get_timezone_from_coordinates,
-    expand_weather_dataframe,
-    flatten_weather_data,
-    add_sensor_units,
-    check_timestamp_match,
-    add_timezone_from_coordinates,
-    parse_sensor_schema,
-)
-#fix this later
-# __version__ = importlib.metadata.version(__package__)
+from . import utils
+from . import models
 __author__ = "Zach Milgate"
 __email__ = "zach.milgate@example.com"
 __description__ = "BioScout Technical Challenge Implementation"
 
-# List all modules and specific functions/classes to be exposed
+# List all modules to be exposed
 __all__ = [ 
     'models',
-    'read_csv_file',
-    'save_csv_file',
-    'identify_header',
-    'combine_csv_files',
-    'find_csv_files',
-    'read_json_file',
-    'merge_weather_data',
-    'get_na_rows',
-    'expand_extra_information',
-    # 'get_timezone_from_utc_offset',
-    'get_timezone_from_coordinates',
-    'expand_weather_dataframe',
-    'flatten_weather_data',
-    'add_sensor_units',
-    'check_timestamp_match',
-    'add_timezone_from_coordinates',
-    'parse_sensor_schema',
+    'utils'
 ]
--- src/bioscout_tech_challenge/utils/__init__.py
+++ src/bioscout_tech_challenge/utils/__init__.py
@@ -5,45 +5,7 @@ bioscout.utils
 Utility functions and classes for the BioScout technical challenge.
 """
 
-from .file_operations import (
-    read_csv_file,
-    save_csv_file,
-    identify_header,
-    combine_csv_files,
-    find_csv_files,
-    read_json_file,
-    parse_sensor_schema
-)
-from .weather import (
-    merge_weather_data,
-    get_na_rows,
-    expand_extra_information,
-    # get_timezone_from_utc_offset,
-    get_timezone_from_coordinates,
-    expand_weather_dataframe,
-    flatten_weather_data,
-    add_sensor_units,
-    check_timestamp_match,
-    add_timezone_from_coordinates,
-)
+from . import file_operations
+from . import weather
 
-
-__all__ = [
-    'read_csv_file',
-    'save_csv_file',
-    'identify_header',  
-    'merge_weather_data',
-    'get_na_rows',
-    'combine_csv_files',
-    'find_csv_files',
-    'expand_extra_information',
-    # 'get_timezone_from_utc_offset',
-    'get_timezone_from_coordinates',
-    'expand_weather_dataframe',
-    'flatten_weather_data',
-    'add_sensor_units',
-    'check_timestamp_match',
-    'read_json_file',
-    'parse_sensor_schema',
-    'add_timezone_from_coordinates'
-] 
+__all__ = ['file_operations', 'weather'] 
